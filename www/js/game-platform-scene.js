b06dca5e07789dc6e73e1974d16202af3ec2c545
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-04-27 17:34:00 -0400

    feat: setup the player class and basic functionality

--- www/js/game-platform-scene.js
+++ www/js/game-platform-scene.js
@@ -1,3 +1,5 @@
+import Player from "./game-player.js"
+
 export default class PlatformScene extends Phaser.Scene {
 	preload() {
 		this.load.image("background", "../assets/images/background-platformer.png");
@@ -18,8 +20,18 @@ export default class PlatformScene extends Phaser.Scene {
 			this.sys.game.config.width / backgroundImage.width,
 			this.sys.game.config.height / backgroundImage.height
 		);
+
 		const map = this.make.tilemap({ key: "map" });
 		const tiles = map.addTilesetImage("tileset-platformer-test", "tiles");
-		map.createLayer("ground", tiles);
+		this.groundLayer = map.createLayer("ground", tiles);
+
+		this.player = new Player(this, 150, 150);
+		this.physics.world.addCollider(this.player.sprite, this.groundLayer);
+		//this.groundLayer.setCollisionByProperty({ collides: true });
+		this.groundLayer.setCollisionByExclusion([0]);
+	}
+
+	update() {
+		this.player.update();
 	}
 }
--- www/js/game-player.js
+++ www/js/game-player.js
@@ -1,15 +1,69 @@
 export default class Player {
 	constructor(scene, x, y) {
 		this.scene = scene;
-		// content goes here
+		const anims = scene.anims;
+		anims.create({
+			key: "player-idle",
+			frames: anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+			frameRate: 3,
+			repeat: -1,
+		});
+		anims.create({
+			key: "player-run",
+			frames: anims.generateFrameNumbers("player", { start: 8, end: 15 }),
+			frameRate: 12,
+			repeat: -1,
+		});
+		this.sprite = scene.physics.add
+			.sprite(x, y, "player", 0)
+			.setDrag(1000, 0)
+			.setMaxVelocity(300, 400)
+			.setSize(18, 24)
+			.setOffset(7, 9);
+		const { LEFT, RIGHT, UP, W, A, D } = Phaser.Input.Keyboard.KeyCodes;
+		this.keys = scene.input.keyboard.addKeys({
+			left: LEFT,
+			right: RIGHT,
+			up: UP,
+			w: W,
+			a: A,
+			d: D,
+		});
 	}
 
 	freeze() {
+		this.sprite.body.moves = false;
 	}
 
 	update() {
+		const { keys, sprite } = this;
+		const onGround = sprite.body.blocked.down;
+		const acceleration = onGround ? 600 : 200;
+
+		if (keys.left.isDown || keys.a.isDown) {
+			sprite.setAccelerationX(-acceleration);
+			sprite.setFlipX(true);
+		} else if (keys.right.isDown || keys.d.isDown) {
+			sprite.setAccelerationX(acceleration);
+			sprite.setFlipX(false);
+		} else {
+			sprite.setAccelerationX(0);
+		}
+
+		if (onGround && (keys.up.isDown || keys.w.isDown)) {
+			sprite.setVelocityY(-500);
+		}
+
+		if (onGround) {
+			if (sprite.body.velocity.x !== 0) sprite.anims.play("player-run", true);
+			else sprite.anims.play("player-idle", true);
+		} else {
+			sprite.anims.stop();
+			sprite.setTexture("player", 10);
+		}
 	}
 
 	destroy() {
+		this.sprite.destroy();
 	}
 }
