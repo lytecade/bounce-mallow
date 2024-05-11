9fb5d97b06d290c35adb2eaf471cbd1a7a2fef39
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-11 00:52:18 -0400

    feat: add constants file

---
+++ www/js/constants.js
@@ -0,0 +1,5 @@
+export const AUDIO_RESOURCES_STD = new Map([
+	['sfx-jump', { type: 'audio', name: 'sfx-jump', ext: 'wav', options: {} }],
+	['sfx-lose', { type: 'audio', name: 'sfx-lose', ext: 'wav', options: {} }]
+]);
+
