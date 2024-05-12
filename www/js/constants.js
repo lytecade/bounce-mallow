d0718831e5ab8f0b38c9b73b6315a86cf590ed9d
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-11 21:13:15 -0400

    feat: add background resource export

--- www/js/constants.js
+++ www/js/constants.js
@@ -3,3 +3,8 @@ export const AUDIO_RESOURCES_STD = new Map([
 	['sfx-lose', { type: 'audio', name: 'sfx-lose', ext: 'wav', options: {} }]
 ]);
 
+export const BACKGROUND_RESOURCES_HILLS = new Map([
+	["background-hills", { type: "images", name "background-hills", ext: "png", options: {}]},
+	["background-hills-front", { type: "images", name "background-hills-front", ext: "png", options: {}]}
+]);
+
