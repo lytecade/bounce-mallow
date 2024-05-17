b70033b22e5e8f8b3cf236903ebfb1c72c31a84d
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-17 16:48:56 -0400

    chore: correct spacing in files

--- www/js/game.js
+++ www/js/game.js
@@ -2,141 +2,141 @@ import Utils from "/js/utils.js";
 import { BASE_RESOURCES, BACKGROUND_RESOURCES_HILLS } from "/js/constants.js";
 
 class Player {
-	constructor(scene, x, y, sys) {
-		this.scene = scene;
-		this.sys = sys;
-		const anims = scene.anims;
-		anims.create({
-			key: "player-idle",
-			frames: anims.generateFrameNumbers("sprite-player", { start: 0, end: 3 }),
-			frameRate: 3,
-			repeat: -1,
-		});
-		anims.create({
-			key: "player-run",
-			frames: anims.generateFrameNumbers("sprite-player", { start: 8, end: 15 }),
-			frameRate: 12,
-			repeat: -1,
-		});
-		this.sprite = scene.physics.add
-			.sprite(x, y, "sprite-player", 0)
-			.setDrag(1000, 0)
-			.setMaxVelocity(150, 500)
-			.setSize(9, 12)
-			.setOffset(3, 4);
-		const { LEFT, RIGHT, UP, W, A, D } = Phaser.Input.Keyboard.KeyCodes;
-		this.keys = scene.input.keyboard.addKeys({
-			left: LEFT,
-			right: RIGHT,
-			up: UP,
-			w: W,
-			a: A,
-			d: D,
-		});
-	}
-	freeze() {
-		this.sprite.body.moves = false;
-	}
-	update() {
-		const { keys, sprite } = this;
-		if(sprite.body !== undefined) {
-			const onGround = sprite.body.blocked.down;
-			const acceleration = onGround ? 75 : 50;
-			let canJump = true;
-			if (keys.left.isDown || keys.a.isDown) {
-				sprite.setAccelerationX(-acceleration);
-				sprite.setFlipX(true);
-			} else if (keys.right.isDown || keys.d.isDown) {
-				sprite.setAccelerationX(acceleration);
-				sprite.setFlipX(false);
-			} else {
-				sprite.setAccelerationX(0);
-			}
-			if (onGround && (keys.up.isDown || keys.w.isDown)) {
-				if(this.canJump === true) {
-					sprite.setVelocityY(-250);
-					this.scene.jumpSound.play();
-					this.canJump = false;
-				}
-			} 
-			if (onGround && (keys.up.isUp && keys.w.isUp)) {
-				this.canJump = true;
-			}
-			if (onGround) {
-				if (sprite.body.velocity.x !== 0) {
-					sprite.anims.play("player-run", true);
-				}
-				else { 
-					sprite.anims.play("player-idle", true);
-				}
-			} else {
-				sprite.anims.stop();
-				sprite.setTexture("sprite-player", 9);
-			}
-			if (sprite.body.velocity.y >= 500) {
-				this.destroy();
-			}
-		}	
-	}
-	destroy() {
-		this.scene.loseSound.play();
-		this.sprite.destroy();
-		this.scene.resetSceneCall();	
-	}
+    constructor(scene, x, y, sys) {
+        this.scene = scene;
+        this.sys = sys;
+        const anims = scene.anims;
+        anims.create({
+            key: "player-idle",
+            frames: anims.generateFrameNumbers("sprite-player", { start: 0, end: 3 }),
+            frameRate: 3,
+            repeat: -1,
+        });
+        anims.create({
+            key: "player-run",
+            frames: anims.generateFrameNumbers("sprite-player", { start: 8, end: 15 }),
+            frameRate: 12,
+            repeat: -1,
+        });
+        this.sprite = scene.physics.add
+            .sprite(x, y, "sprite-player", 0)
+            .setDrag(1000, 0)
+            .setMaxVelocity(150, 500)
+            .setSize(9, 12)
+            .setOffset(3, 4);
+        const { LEFT, RIGHT, UP, W, A, D } = Phaser.Input.Keyboard.KeyCodes;
+        this.keys = scene.input.keyboard.addKeys({
+            left: LEFT,
+            right: RIGHT,
+            up: UP,
+            w: W,
+            a: A,
+            d: D,
+        });
+    }
+    freeze() {
+        this.sprite.body.moves = false;
+    }
+    update() {
+        const { keys, sprite } = this;
+        if(sprite.body !== undefined) {
+            const onGround = sprite.body.blocked.down;
+            const acceleration = onGround ? 75 : 50;
+            let canJump = true;
+            if (keys.left.isDown || keys.a.isDown) {
+                sprite.setAccelerationX(-acceleration);
+                sprite.setFlipX(true);
+            } else if (keys.right.isDown || keys.d.isDown) {
+                sprite.setAccelerationX(acceleration);
+                sprite.setFlipX(false);
+            } else {
+                sprite.setAccelerationX(0);
+            }
+            if (onGround && (keys.up.isDown || keys.w.isDown)) {
+                if(this.canJump === true) {
+                    sprite.setVelocityY(-250);
+                    this.scene.jumpSound.play();
+                    this.canJump = false;
+                }
+            } 
+            if (onGround && (keys.up.isUp && keys.w.isUp)) {
+                this.canJump = true;
+            }
+            if (onGround) {
+                if (sprite.body.velocity.x !== 0) {
+                    sprite.anims.play("player-run", true);
+                }
+                else { 
+                    sprite.anims.play("player-idle", true);
+                }
+            } else {
+                sprite.anims.stop();
+                sprite.setTexture("sprite-player", 9);
+            }
+            if (sprite.body.velocity.y >= 500) {
+                this.destroy();
+            }
+        }    
+    }
+    destroy() {
+        this.scene.loseSound.play();
+        this.sprite.destroy();
+        this.scene.resetSceneCall();    
+    }
 }
 
 class PlatformScene extends Phaser.Scene {
-	preload() {
-		Utils.loadResources(this, BASE_RESOURCES);
-		Utils.loadResources(this, BACKGROUND_RESOURCES_HILLS);
-	}
-	create() {	
-		Utils.createBackgrounds(this, 1, 'background-hills', 0);
-		Utils.createBackgrounds(this, 3, 'background-hills-front', 0.25);
+    preload() {
+        Utils.loadResources(this, BASE_RESOURCES);
+        Utils.loadResources(this, BACKGROUND_RESOURCES_HILLS);
+    }
+    create() {    
+        Utils.createBackgrounds(this, 1, 'background-hills', 0);
+        Utils.createBackgrounds(this, 3, 'background-hills-front', 0.25);
 
-		this.player = new Player(this, 32, 118, this.sys);
-		
-		const map = this.make.tilemap({ key: "tilemap-platform" });
-		const tiles = map.addTilesetImage("tileset-platform", "tileset-platform");
+        this.player = new Player(this, 32, 118, this.sys);
+        
+        const map = this.make.tilemap({ key: "tilemap-platform" });
+        const tiles = map.addTilesetImage("tileset-platform", "tileset-platform");
 
-		this.groundLayer = map.createLayer("ground", tiles);
-		this.physics.world.addCollider(this.player.sprite, this.groundLayer);
-		this.groundLayer.setCollisionByProperty({ collides: true });
+        this.groundLayer = map.createLayer("ground", tiles);
+        this.physics.world.addCollider(this.player.sprite, this.groundLayer);
+        this.groundLayer.setCollisionByProperty({ collides: true });
 
-		this.jumpSound = this.sound.add('sfx-jump');
-		this.loseSound = this.sound.add('sfx-lose');
-		
-		this.cameras.main.startFollow(this.player.sprite);
-		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-	}
-	update() {
-		this.player.update();
-	}
-	resetScene() {
-		this.scene.restart();
-	}
-	resetSceneCall() {
-		this.time.delayedCall(2000, this.resetScene, [], this);
-	}
+        this.jumpSound = this.sound.add('sfx-jump');
+        this.loseSound = this.sound.add('sfx-lose');
+        
+        this.cameras.main.startFollow(this.player.sprite);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    }
+    update() {
+        this.player.update();
+    }
+    resetScene() {
+        this.scene.restart();
+    }
+    resetSceneCall() {
+        this.time.delayedCall(2000, this.resetScene, [], this);
+    }
 }
 
 const config = {
-	parent: "game",
-	type: Phaser.AUTO,
-	width: 240,
-	height: 150,
-	pixelArt: true,
-	scene: PlatformScene,
-	physics: {
-		default: "arcade",
-		arcade: {
-			gravity: { y: 1000 },
-		},
-	},
-	scale: {
-		mode: Phaser.Scale.ENVELOP,
-		autoCenter: Phaser.Scale.CENTER_BOTH,
-	}
+    parent: "game",
+    type: Phaser.AUTO,
+    width: 240,
+    height: 150,
+    pixelArt: true,
+    scene: PlatformScene,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 1000 },
+        },
+    },
+    scale: {
+        mode: Phaser.Scale.ENVELOP,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    }
 };
 
 const game = new Phaser.Game(config);
--- www/js/utils.js
+++ www/js/utils.js
@@ -1,34 +1,34 @@
 export default class Utils {
-	static loadResources = (scene, resourceCollection) => {
-		for (const [key, value] of resourceCollection) {
-			let resourcePath = "/assets/" + value.type + "/" + value.name + "." + value.ext;
-			console.log(resourcePath);
-			switch(value.type) {
-				case "audio":
-					scene.load.audio(value.name, resourcePath);
-					break;
-				case "images":
-				case "tilesets":
-					scene.load.image(value.name, resourcePath);
-					break;
-				case "spritesheets":
-					scene.load.spritesheet(value.name, resourcePath, value.options);
-					break;
-				case "tilemaps":
-					scene.load.tilemapTiledJSON(value.name, resourcePath);
-					break;
-				default:
-					break;
-			}
-		}
-	}
-	static createBackgrounds = (scene, count, texture, scrollFactor) => {
-		let trackingXValue = 0;
-		for(let i = 0; i < count; ++i) {
-			const currentImage = scene.add.image(trackingXValue, 0, texture)
-				.setOrigin(0, 0)
-				.setScrollFactor(scrollFactor);
-			trackingXValue += currentImage.width;
-		}	
-	}
+    static loadResources = (scene, resourceCollection) => {
+        for (const [key, value] of resourceCollection) {
+            let resourcePath = "/assets/" + value.type + "/" + value.name + "." + value.ext;
+            console.log(resourcePath);
+            switch(value.type) {
+                case "audio":
+                    scene.load.audio(value.name, resourcePath);
+                    break;
+                case "images":
+                case "tilesets":
+                    scene.load.image(value.name, resourcePath);
+                    break;
+                case "spritesheets":
+                    scene.load.spritesheet(value.name, resourcePath, value.options);
+                    break;
+                case "tilemaps":
+                    scene.load.tilemapTiledJSON(value.name, resourcePath);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    static createBackgrounds = (scene, count, texture, scrollFactor) => {
+        let trackingXValue = 0;
+        for(let i = 0; i < count; ++i) {
+            const currentImage = scene.add.image(trackingXValue, 0, texture)
+                .setOrigin(0, 0)
+                .setScrollFactor(scrollFactor);
+            trackingXValue += currentImage.width;
+        }    
+    }
 }
