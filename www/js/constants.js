feee3a670f5acbd5b37c921067c5803a36a5b6d8
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-28 18:17:14 -0400

    feat: update constants to include new sprite resource

--- www/js/constants.js
+++ www/js/constants.js
@@ -7,6 +7,12 @@ export const BASE_RESOURCES = new Map([
         margin:0,
         spacing:0
     } }],
+    ["sprite-enemy-spike", { type: "spritesheets", name: "sprite-enemy-spike", ext: "png", options: {
+        frameWidth:8,
+        frameHeight:8,
+        margin:0,
+        spacing:0
+    } }],
     ["tileset-platform", { type: "tilesets", name: "tileset-platform", ext: "png", options: {} }],
     ["tilemap-platform", { type: "tilemaps", name: "tilemap-platform", ext: "json", options: {} }]
 ]);
