7ac6d2851d28a2cfafc5bf9ef6a60117823a8193
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-29 17:07:38 -0400

    feat: add boilerplate for enemy animation

--- www/js/game.js
+++ www/js/game.js
@@ -87,15 +87,18 @@ class PlatformScene extends Phaser.Scene {
         Utils.loadResources(this, BACKGROUND_RESOURCES_HILLS);
     }
     create() {    
-        Utils.createBackgrounds(this, 1, 'background-hills', 0);
-        Utils.createBackgrounds(this, 3, 'background-hills-front', 0.25);
+        Utils.createBackgrounds(this, 1, "background-hills", 0);
+        Utils.createBackgrounds(this, 3, "background-hills-front", 0.25);
  
+        const anims = this.anims;
+
         this.player = new Player(this, 16, 56, this.sys);
         const map = this.make.tilemap({ key: "tilemap-platform" });
         const tiles = map.addTilesetImage("tileset-platform", "tileset-platform");
 
         this.groundLayer = map.createLayer("ground", tiles);
         this.loseLayer = map.createLayer("lose", tiles);
+
         this.physics.world.addCollider(this.player.sprite, this.groundLayer);
         this.groundLayer.setCollisionByProperty({ collides: true });
         this.playerLoseCollider = this.physics.add.overlap(
@@ -111,6 +114,23 @@ class PlatformScene extends Phaser.Scene {
             },
             this
         );
+       
+        this.enemies = [];
+ 
+        this.enemyLayer = map.getObjectLayer("enemy");
+        this.enemyLayer.objects.forEach(enemyObject => {
+            const enemy = this.physics.add.sprite(enemyObject.x, enemyObject.y, "sprite-enemy-spike");
+            enemy.body.setSize(16, 16);
+            this.enemies.push(enemy);
+
+            const currentAnims = anims;
+            currentAnims.create({
+                key: 'enemyIdle',
+                frames: currentAnims.generateFrameNumbers("sprite-enemy-spike", { start: 0, end: 3 }),
+                frameRate: 3,
+                repeat: -1
+            });
+        });
 
         Utils.createSounds(this, BASE_RESOURCES);
 
@@ -120,7 +140,6 @@ class PlatformScene extends Phaser.Scene {
     update() {
         this.player.update();
     }
-
     resetSceneCall() {
         this.time.delayedCall(2000, () => { this.scene.restart(); }, [], this);
     }
