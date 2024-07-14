a0a003ae555c0bb75fa505eb312031134788bdf6
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-07-13 22:47:35 -0400

    feat: add background front hills to dynamic scene

--- www/js/dynamic-platform-scene.js
+++ www/js/dynamic-platform-scene.js
@@ -14,6 +14,7 @@ export default class DynamicPlatformScene extends Phaser.Scene {
         this.chunkWidth = TileSettings.TileChunkDefaultSize;
         this.activeChunks = TileSettings.TileChunkDefaultActive; 
         Utils.createBackgrounds(this, 1, "background-hills", 0);
+        Utils.createBackgrounds(this, 3, "background-hills-front", 0.25);
         Utils.createSceneAttributes(this, BASE_RESOURCES);
         this.player = new Player(this, this.chunkWidth, 10);
         this.generateInitialChunks();
