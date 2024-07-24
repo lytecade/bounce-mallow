10de007d235b24b97b2563da872da4c2957a1ec1
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-07-23 23:23:32 -0400

    feat: add return map for chunk lose layers

--- www/js/dynamic-platform-scene.js
+++ www/js/dynamic-platform-scene.js
@@ -65,4 +65,7 @@ export default class DynamicPlatformScene extends Phaser.Scene {
             this.removeOldestChunk();
         }
     }
+    getChunkLoseLayers() {
+        return this.chunks.map(chunk => chunk.loseLayer);
+    }
 }
