4f0e1a12dac070f1425dff404a6036610c415a99
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-09-18 18:10:10 -0400

    feat: add dynamic enemy class for dynamic scene

---
+++ www/js/dynamic-enemy.js
@@ -0,0 +1,67 @@
+export default class Enemy {
+    constructor(chunk, scene, x, y) {
+        this.scene = scene;
+        this.chunk = chunk;
+        const anims = scene.anims;
+        anims.create({
+            key: "enemy-idle",
+            frames: anims.generateFrameNumbers("sprite-enemy-spike", {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 4,
+            repeat: -1,
+        });
+        anims.create({
+            key: "enemy-walk",
+            frames: anims.generateFrameNumbers("sprite-enemy-spike", {
+                start: 4,
+                end: 7
+            }),
+            frameRate: 4,
+            repeat: -1
+        });
+        this.sprite = chunk.physics.add.sprite(x, y, "sprite-enemy-spike", 0)
+            .setDrag(100, 0)
+            .setSize(8, 8);
+        this.spriteCollider = this.chunk.physics.world.addCollider(this.sprite, chunk.groundLayer);
+        this.isStationary = true;
+        this.isForward = true;
+    }
+    update(time, delta) {
+        if (!this.moveTimer) {
+            this.moveTimer = time;
+        }
+        if (time - this.moveTimer > 4000) {
+            this.moveTimer = time;
+            this.changeEnemyActivity();
+        }
+        this.handleEnemyActivity();
+    }
+    changeEnemyActivity() {
+        if (this.isStationary === true) {
+           this.isStationary = false;
+           this.isForward = (this.isForward === true) ? false : true;
+        } else {
+           this.isStationary = true;
+        }
+    } 
+    handleEnemyActivity() {
+        if (this.isStationary === true) {
+            this.sprite.anims.play("enemy-idle", true);
+            this.sprite.setAccelerationX(0);
+        } else {
+            this.sprite.anims.play("enemy-walk", true);
+            if (this.isForward === true) {
+                this.sprite.setAccelerationX(1);
+            } else {
+                this.sprite.setAccelerationX(-1);
+            }
+        }
+        if (this.isForward === true) {
+	    this.sprite.setFlipX(false);
+        } else {
+	    this.sprite.setFlipX(true);
+        }
+    }
+}
