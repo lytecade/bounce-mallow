98b78c1440d529857b53e225a31ca076420b6972
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-10-10 13:31:52 -0400

    refactor: cleanup dynamic enemy class

--- www/js/dynamic-enemy.js
+++ www/js/dynamic-enemy.js
@@ -2,53 +2,54 @@ export default class Enemy {
     constructor(chunk, scene, x, y) {
         this.scene = scene;
         this.chunk = chunk;
-        this.sprite = scene.physics.add.sprite(x, y, "sprite-enemy-spike", 0)
-            .setDrag(100, 0)
-            .setSize(8, 8);
+        this.sprite = scene.physics.add.sprite(x, y, "sprite-enemy-spike", 0).setDrag(100, 0).setSize(8, 8);
         this.spriteCollider = scene.physics.world.addCollider(this.sprite, chunk.groundLayer);
+        this.setupOverlap();
+        this.isStationary = true;
+        this.isForward = true;
+        this.activityFactor = Math.random();
+        this.moveTimer = null;
+    }
+
+    setupOverlap() {
+        const { scene } = this;
         scene.physics.add.overlap(
             this.sprite,
             scene.player.sprite,
             () => {
-	        scene.loseSequenceActive = true;
-	        scene.loseSequenceFromEnemy = true;
+                scene.loseSequenceActive = true;
+                scene.loseSequenceFromEnemy = true;
             },
             null,
             this
         );
-        this.isStationary = true;
-        this.isForward = true;
-        this.activityFactor = Math.random()
     }
-    update(time, delta) {
-        if (!this.moveTimer) {
+
+    update(time) {
+        if (!this.moveTimer) this.moveTimer = time;
+
+        if (this.shouldChangeState(time)) {
+            this.toggleMovement();
             this.moveTimer = time;
         }
-        if (time - this.moveTimer > 4000 || (this.activityFactor > 0 && this.activityFactor < 0.5)) {
-            this.activityFactor = -1;
-            this.moveTimer = time;
-            if (this.isStationary === true) {
-               this.isStationary = false;
-               this.isForward = (this.isForward === true) ? false : true;
-            } else {
-               this.isStationary = true;
-            }
-        }
-        if (this.isStationary === true) {
-            this.sprite.anims.play("enemy-idle", true);
-            this.sprite.setVelocityX(0);
-        } else {
-            this.sprite.anims.play("enemy-walk", true);
-            if (this.isForward === true) {
-                this.sprite.setVelocityX(5);
-            } else {
-                this.sprite.setVelocityX(-5);
-            }
-        }
-        if (this.isForward === true) {
-	    this.sprite.setFlipX(false);
-        } else {
-	    this.sprite.setFlipX(true);
-        }
+        this.updateSprite();
+    }
+
+    shouldChangeState(time) {
+        return time - this.moveTimer > 4000 || (this.activityFactor > 0 && this.activityFactor < 0.5);
+    }
+
+    toggleMovement() {
+        this.isStationary = !this.isStationary;
+        if (!this.isStationary) this.isForward = !this.isForward;
+        else this.activityFactor = -1;
+    }
+
+    updateSprite() {
+        const velocity = this.isStationary ? 0 : (this.isForward ? 5 : -5);
+        this.sprite.setVelocityX(velocity);
+        this.sprite.anims.play(this.isStationary ? "enemy-idle" : "enemy-walk", true);
+        this.sprite.setFlipX(!this.isForward);
     }
 }
+
