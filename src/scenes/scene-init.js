5af8ae200c2cb34976060cd9132ee527731dadbe
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-11-28 17:01:58 -0500

    feat: add init scene with title and buttons

--- src/scenes/scene-init.js
+++ src/scenes/scene-init.js
@@ -8,6 +8,9 @@ export default class InitScene extends Phaser.Scene {
     }
     create() {
         Resources.createBackgrounds(this, "image-background");
+		const centerX = this.cameras.main.width / 2;
+        this.titleBanner = this.add.image(centerX, 20, "image-title").setOrigin(0.5, 0).setScrollFactor(0).setDepth(100);
+        this.buttonPlay = this.add.image(centerX, 40, "image-playbutton").setOrigin(0.5, 0).setScrollFactor(0).setDepth(100);
+        this.buttonGuide = this.add.image(centerX, 50, "image-guidebutton").setOrigin(0.5, 0).setScrollFactor(0).setDepth(100);
     }
-    update() {
-    }
+}
